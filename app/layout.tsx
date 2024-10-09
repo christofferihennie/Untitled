3e879d0fd966d404dab2d28f89324d3410ab2c87
Christoffer Hennie <christoffer.hennie@outlook.com>
2024-10-10 01:36:44 +0200

    Updated to follow prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,43 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Public_Sans, Playfair_Display } from 'next/font/google';
-import "./globals.scss";
+import '@/styles/globals.scss';
+import Navigation from '@/components/navigation';
 
 export const metadata: Metadata = {
-  title: "Untitled",
-  description: "The untitled bok app",
+    title: 'Untitled',
+    description: 'The untitled bok app',
 };
 
 const public_sans = Public_Sans({
-  subsets: ['latin'],
-  variable: '--font-public-sans',
-  display: 'swap',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-public-sans',
 });
 
 const playfair_display = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-playfair-display',
-  display: 'swap',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-playfair-display',
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="no" className={`${playfair_display.variable} ${public_sans.variable}`}>
-      <head>
-        <link rel="icon" type="image/png" href="favicon/favicon-48x48.png" sizes="48x48" />
-        <link rel="icon" type="image/svg+xml" href="favicon/favicon.svg" />
-        <link rel="shortcut icon" href="favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="Untitled" />
-      </head>
-      <body>
-        <nav>Hei</nav>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang='no' className={`${playfair_display.variable} ${public_sans.variable}`}>
+            <head>
+                <link rel='icon' type='image/png' href='favicon/favicon-48x48.png' sizes='48x48' />
+                <link rel='icon' type='image/svg+xml' href='favicon/favicon.svg' />
+                <link rel='shortcut icon' href='favicon/favicon.ico' />
+                <link rel='apple-touch-icon' sizes='180x180' href='favicon/apple-touch-icon.png' />
+                <meta name='apple-mobile-web-app-title' content='Untitled' />
+            </head>
+            <body>
+                <Navigation />
+                <main className='border'>{children}</main>
+            </body>
+        </html>
+    );
 }
--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      disallow: '/',
-    },
-    sitemap: 'https://acme.com/sitemap.xml',
-  }
+    return {
+        rules: {
+            userAgent: '*',
+            disallow: '/',
+        },
+        sitemap: 'https://acme.com/sitemap.xml',
+    };
 }
