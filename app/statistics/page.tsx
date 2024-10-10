fba4abae253bf8b4c90a306edb530079444af824
Christoffer Hennie <christoffer.hennie@outlook.com>
2024-10-10 15:14:12 +0200

    Minor adjustments

---
+++ app/statistics/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/header';
+
+export default function StatisticsPage() {
+    return (
+        <>
+            <Header heading="Din statistikk" subheading="Din fabelaktive historie som leser" />
+        </>
+    );
+}
