f48fb7e6a74e7476075d906cc3b29f918e828876
Christoffer Hennie <christoffer.hennie@outlook.com>
2024-10-10 04:23:53 +0200

    Moved to metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,38 @@
 import Navigation from '@/components/navigation';
 import '@/styles/globals.scss';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Playfair_Display, Public_Sans } from 'next/font/google';
 
 export const metadata: Metadata = {
     title: 'Untitled',
     description: 'The untitled bok app',
+    robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+    },
+    appleWebApp: {
+        title: 'Untitled',
+        statusBarStyle: 'black-translucent',
+        startupImage: [
+            '/favicon/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png',
+            {
+                url: '/favicon/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png',
+                media: '(device-width: 768px) and (device-height: 1024px)',
+            },
+        ],
+    },
+    icons: {
+        icon: '/favicon/favicon-32x32.png',
+        apple: '/favicon/apple-touch-icon.png',
+    },
+};
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
 };
 
 const public_sans = Public_Sans({
@@ -27,82 +54,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="no" className={`${playfair_display.variable} ${public_sans.variable}`}>
-            <head>
-                <meta charSet="UTF-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-                />
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                    href="favicon/splash-image.png"
-                />
-
-                {/* Splash screens */}
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_16_Pro_Max_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_16_Pro_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_16_Plus__iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_16__iPhone_15_Pro__iPhone_15__iPhone_14_Pro_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_11__iPhone_XR_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png"
-                />
-                <link
-                    rel="apple-touch-startup-image"
-                    media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                    href="/favicon/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png"
-                />
-                {/* Splash screens */}
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
-                <link rel="manifest" crossOrigin="use-credentials" href="/manifest.json" />
-            </head>
             <body>
                 <Navigation />
                 <main className="border">{children}</main>
