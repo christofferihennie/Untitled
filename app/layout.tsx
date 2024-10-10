f4f2f7be6860e289cc1db1e20234be1e4deaff25
Christoffer Hennie <christoffer.hennie@outlook.com>
2024-10-10 03:41:01 +0200

    Attempt stopping zooming on PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,10 @@ export default function RootLayout({
         <html lang="no" className={`${playfair_display.variable} ${public_sans.variable}`}>
             <head>
                 <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
